e0b210ffca2f9af3f8667886c9bedd8d027f74c5
Koki Yamada <koki.y2764@gmail.com>
2021-10-30 18:59:53 +0900

    Initial Commit

---
+++ dish-management/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dish-management
+//
+//  Created by 山田航輝 on 2021/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
